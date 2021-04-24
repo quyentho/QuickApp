677cd8aa5c4df86f69bf9d212217eb91975d852c
quyentho <thokimquangquyen@gmail.com>
2021-04-24 21:47:30 +0700

    add test project

---
+++ QuickApp.Test/QuickAppTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace QuickApp.Test
+{
+    public class QuickAppTests
+    {
+        [Fact]
+        public void AlwaysTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
