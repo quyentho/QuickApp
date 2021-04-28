0e363d337d4c94e62705b00804f404524b89f69c
quyentho <thokimquangquyen@gmail.com>
2021-04-29 01:08:11 +0700

    config front end

--- QuickApp/ClientApp/src/environments/environment.stag.ts
+++ QuickApp/ClientApp/src/environments/environment.stag.ts
@@ -1,8 +1,9 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://localhost:5001', // Change this to the address of your backend API if different from frontend address
+  baseUrl: 'https://localhost:7002', // Change this to the address of your backend API if different from frontend address
   tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
-  loginUrl: '/login'
+  loginUrl: '/login',
+  port: 4201
 };
 
