ed669c36de96212da89cd67a706f822b85375684
quyentho <thokimquangquyen@gmail.com>
2021-05-13 06:55:19 +0700

    commit

--- QuickApp/ClientApp/src/environments/environment.stag.ts
+++ QuickApp/ClientApp/src/environments/environment.stag.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://54.255.235.122', // Change this to the address of your backend API if different from frontend address
+  baseUrl: 'https://13.213.72.172', // Change this to the address of your backend API if different from frontend address
   tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
   loginUrl: '/login',
 };
