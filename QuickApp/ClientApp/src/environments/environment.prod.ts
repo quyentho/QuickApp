ba30caba5779b36933643c0ab7d688871a19ad5a
quyentho <thokimquangquyen@gmail.com>
2021-05-12 19:49:44 +0700

    change url

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production: true,
-  baseUrl: 'https://13.213.68.47', // Change this to the address of your backend API if different from frontend address
+	baseUrl: 'https://52.221.179.31', // Change this to the address of your backend API if different from frontend address
     tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
--- QuickApp/ClientApp/src/environments/environment.ts
+++ QuickApp/ClientApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://13.213.68.47', // Change this to the address of your backend API if different from frontend address
+    baseUrl: 'https://52.221.179.31', // Change this to the address of your backend API if different from frontend address
     tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
