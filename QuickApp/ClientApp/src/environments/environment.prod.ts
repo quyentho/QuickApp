b0ae16f3d6c35fbb32515c15dab2a34d8251a1c0
quyentho <thokimquangquyen@gmail.com>
2021-05-12 22:02:53 +0700

    connect/token

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
     production: true,
 	baseUrl: 'https://52.221.179.31', // Change this to the address of your backend API if different from frontend address
-    tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
+    tokenUrl: '/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
--- QuickApp/ClientApp/src/environments/environment.ts
+++ QuickApp/ClientApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     baseUrl: 'https://52.221.179.31', // Change this to the address of your backend API if different from frontend address
-    tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
+    tokenUrl: '/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
