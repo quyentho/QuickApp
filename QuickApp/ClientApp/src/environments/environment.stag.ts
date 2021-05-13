696d63819cc599de3a090378b7aea3da13f51276
quyentho <thokimquangquyen@gmail.com>
2021-05-13 15:28:18 +0700

    resolve conflict

--- QuickApp/ClientApp/src/environments/environment.stag.ts
+++ QuickApp/ClientApp/src/environments/environment.stag.ts
@@ -1,13 +1,10 @@
 
 export const environment = {
   production: true,
-<<<<<<< HEAD
+
   baseUrl: 'https://13.213.72.172', // Change this to the address of your backend API if different from frontend address
-  tokenUrl: 'https://13.213.72.172/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
-=======
-  baseUrl: 'https://13.213.72.172/api', // Change this to the address of your backend API if different from frontend address
   tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
->>>>>>> parent of 466fbc6 (default port and no /api client)
+
   loginUrl: '/login',
 };
 
