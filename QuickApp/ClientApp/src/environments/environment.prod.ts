fdbc2042f502539be97e7a4a947d375918affe67
quyentho <thokimquangquyen@gmail.com>
2021-05-13 10:14:27 +0700

    api

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production: true,
-	baseUrl: 'https://18.139.114.138', // Change this to the address of your backend API if different from frontend address
+	baseUrl: 'https://18.139.114.138/api', // Change this to the address of your backend API if different from frontend address
     tokenUrl: '/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
