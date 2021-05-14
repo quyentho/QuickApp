0ee01920fa741c409897787956fe256b956e36f8
quyentho <thokimquangquyen@gmail.com>
2021-05-14 12:50:19 +0700

    change domain name

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production: true,
-	baseUrl: 'https://13.250.100.159', // Change this to the address of your backend API if different from frontend address
+	baseUrl: 'https://quyenprod.byhanh.com', // Change this to the address of your backend API if different from frontend address
     tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
