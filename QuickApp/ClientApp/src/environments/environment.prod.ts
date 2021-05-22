867de36ec7d8b02da972f300a27a0c025357735b
quyentho <thokimquangquyen@gmail.com>
2021-05-22 17:10:23 +0700

    asdf

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production: true,
-	baseUrl: 'https://quyenprod.byhanh.com', // Change this to the address of your backend API if different from frontend address
+	baseUrl: 'http://54.183.102.151', // Change this to the address of your backend API if different from frontend address
     tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
