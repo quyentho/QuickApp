e7cc0fd308f214d5496703b79bd2cb2ce5cf7cf2
quyentho <thokimquangquyen@gmail.com>
2021-05-22 19:41:23 +0700

    token

--- QuickApp/ClientApp/src/environments/environment.stag.ts
+++ QuickApp/ClientApp/src/environments/environment.stag.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
 
   baseUrl: 'https://quyen.byhanh.com', // Change this to the address of your backend API if different from frontend address
-  tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
+  tokenUrl: '/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
 
   loginUrl: '/login',
 };
--- QuickApp/ClientApp/src/environments/environment.ts
+++ QuickApp/ClientApp/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
 
     baseUrl: 'https://quyenprod.byhanh.com', // Change this to the address of your backend API if different from frontend address
-    tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
+    tokenUrl: '/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
